c74e010975ec732874dc3a657930896201b9b9f5
LourdesPF <lourdes.pozo@bosonit.com>
2023-11-17 14:43:31 +0100

    gif app con servicios

--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    CardListComponent,
+    SearchBoxComponent
   ],
   imports: [
     CommonModule
--- 03-gifs-app/src/app/shared/components/sidebar/sidebar.component.ts
+++ 03-gifs-app/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../../gifs/services/gifs.service';
 
 @Component({
   selector: 'shared-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
-export class SidebarComponent { }
+export class SidebarComponent {
+  constructor( private gifsService: GifsService){}
+
+  //private allTags: string[]=[];
+
+  get tags(): string[] {
+    const allTags = this.gifsService.tagsHistory;
+    return allTags;
+  }
+
+}
