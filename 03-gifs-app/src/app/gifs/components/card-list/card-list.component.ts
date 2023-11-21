614a1ddc41de40535b0c2d7b57dd880c2032d747
LourdesPF <lourdes.pozo@bosonit.com>
2023-11-21 10:04:29 +0100

    Fin Seccion 8

--- 03-gifs-app/src/app/gifs/components/card-list/card-list.component.ts
+++ 03-gifs-app/src/app/gifs/components/card-list/card-list.component.ts
@@ -4,13 +4,10 @@ import { Gif } from '../../interfaces/gifs.interfaces';
 @Component({
   selector: 'gifs-card-list',
   templateUrl: './card-list.component.html',
-  styleUrls: ['./card-list.component.css']
 })
 export class CardListComponent {
 
   @Input()
   public gifs: Gif[] = [];
-
-
 
 }
--- 03-gifs-app/src/app/gifs/gifs.module.ts
+++ 03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
+
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { CardListComponent } from './components/card-list/card-list.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardComponent } from './components/card/card.component';
+
 
 
 
@@ -11,10 +15,12 @@ import { SearchBoxComponent } from './components/search-box/search-box.component
   declarations: [
     HomePageComponent,
     CardListComponent,
+    CardComponent,
     SearchBoxComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomePageComponent,
