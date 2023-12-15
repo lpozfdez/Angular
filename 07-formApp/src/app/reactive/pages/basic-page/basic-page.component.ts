46c0b6dd2bb3d718377017df89a920f25dbbe142
LourdesPF <lourdes.pozo@bosonit.com>
2023-12-15 12:38:52 +0100

    Formularios reactivos inicio

--- 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./basic-page.component.css']
 })
 export class BasicPageComponent {
+
+  //Una forma de crearlo
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl('', [], []),//[] son validaciones
+  //   price: new FormControl(0, [], []),
+  //   storage: new FormControl(0, [], []),
+
+  // });
+
+  //Otra forma de crearlo
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3) ]],
+    price: ['0', [ Validators.required, Validators.min(0) ]],
+    inStorage: ['0',[ Validators.required, Validators.min(0) ]],
+  })
+
+  constructor( private fb:FormBuilder ) {}
+
+  onSave():void{
+
+    if( this.myForm.invalid) return;
+    console.log(this.myForm.value);
+
+  }
+
 
 }
